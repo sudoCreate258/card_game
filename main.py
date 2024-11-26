fcbfe33390c6762cd0eee899fd1416ab3551a6ca
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2024-11-26 12:27:45 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from objects import Player, Game
+
+def main():
+    p1 = Player("p1")   # TODO create player
+    g1 = Game("21")     # TODO create game
+    g1.start()          # TODO start game
+
+if __name__ == "__main__":
+    main()
+    
