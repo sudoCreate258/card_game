15b678780f0401390c1aaf5b8a871968a6c6c143
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2024-11-26 13:45:03 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@
 from objects import Player, Game
 
 def main():
-    p1 = Player("p1")   # TODO create player
-    g1 = Game("21")     # TODO create game
-    g1.start()          # TODO start game
+    p1 = Player("p1")       # TODO create player
+    g1 = Game_21(p1)        # TODO create game
+    g1.start()              # TODO start game
 
 if __name__ == "__main__":
     main()
